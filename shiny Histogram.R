0716d73ddc03a6796d2acaea882bb7b1a057b2bc
Ankit Yadav <Ankityadavgci2424@gmail.com>
2026-01-12 20:32:56 +0530

    Add files via upload

---
+++ shiny Histogram.R
@@ -0,0 +1,16 @@
+#Basic function/code
+
+Comments;- By using hastag we can convert the line into a comment,#.
+ls();- This function generate all the variables in the console.(make a list of all variables)
+rm();- This is used for removing the specific objects/variable in the environment.
+Ctrl+enter;- This shortcut key to run and execute the code in the R scripts.
+Ctrl+L;- used for cleaning the console panel.
+rm(list=ls());- used for removing all objects in the environment.
+getwd();-Is used to get working directory.(location where our code is saving).
+setwd("C:\\Users\\HP\\OneDrive\\Desktop\\Data Science\\R Programming\\code")
+setwd("location");- By using this function we can change our location where we have to store.
+install.packages(" ");- This is use to install the package provided in R studio.
+library();-This is use to call the package. 
+
+
+Add<-function(X,Y){(X+Y) return(X-Y) temp<<-(X*Y) return((X/Y))}
